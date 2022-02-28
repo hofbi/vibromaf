958fa0a0b0a3b18e0ef61ef374aae4650ab0aa39
Markus Hofbauer <markus.hofbauer@tum.de>
2022-02-28 18:18:03 +0100

    bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ REQUIREMENTS = ROOT_DIR / "requirements.txt"
 
 setuptools.setup(
     name="vibromaf",
-    version="0.0.3",
+    version="0.0.5",
     author="Markus Hofbauer, Andreas Noll",
     author_email="name.surname@tum.de",
     description="Vibrotactile quality metrics and metric fusion",
